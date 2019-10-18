f8b4c3f3315ec860cc4cf92ddc7d564d61489a24
Kalinss <Kalinss16@gmail.com>
2019-10-18 20:38:12 +0400

    add IO monad comment for function Movement Elements Monad

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -2,39 +2,45 @@ import * as R from 'ramda'
 import FL from 'ramda-fantasy'
 const IO = FL.IO;
 
-const getCssForItem = (item) => {
+//getLeftPosition:: m -> int // (for position left)
+const getLeftPosition = (item) => item.map(el => el.offset().left).runIO();  // left position sidebar
+//mainSectionPosition:: m(document) -> int // (for position top)
+const mainSectionPosition = (doc) => doc.map(el => el.children().find('section').first().offset().top).runIO();
+//getConfig:: int -> obj
+const getConfig = (left) => {
     return {
-        left: $(item).offset().left,
+        left: left,
         width: 265,
         top: 0,
         position: 'fixed'
     }
 };
-
-const scrollTopPosition = (a) => $(a).scrollTop();
+// getConfigThis :: item -> obj
+const getConfigThis = R.compose(getConfig, getLeftPosition);
+//fixedItem:: (m,option) -> sideEffect
+const fixedItem = (m, option) => m.map(el => $(R.head(el)).css(option)).runIO();
+//notFixed:: m->sideEffect
+const notFixed = (m) => m.map(el => $(R.head(el)).css('position', 'inherit')).runIO();
+//scrollTopPosition:: m -> int //(top position for sidebar)
+const scrollTopPosition = (m) => m.map((item) => item.scrollTop()).runIO();
+//equalsAandB::(a,b)->bool
 const equalsAandB = (a, b) => a > b;
 
-const eventScroll = (item, position, document, option) => {
+
+const eventScroll = (sidebarM, position, documentM, option) => {
     const result = equalsAandB(
-        scrollTopPosition(document),
+        scrollTopPosition(documentM),
         position
     );
-    const fixedItem = (item, option) => {
-        $(item).css({...option})
-    };
-    const notFixed = (item, option) => {
-        $(item).css('position', 'inherit')
-    };
-    result ? fixedItem(item, option) : notFixed(item);
+    result ? fixedItem(sidebarM, option) : notFixed(sidebarM);
 };
-
 export default function (sidebar) {
-    const option = getCssForItem(sidebar);
-    const there = sidebar;
-    const firstSection = $(document).children().find('section').first();
-    const constPosition = firstSection.offset().top;
+    const sidebarM = IO(() => $(sidebar));
+    const documentM = IO(() => $(document));
+    const option = getConfigThis(sidebarM);
+    const constPosition = mainSectionPosition(documentM);
 
     $(document).on('scroll', () => {
-        eventScroll(there, constPosition, document, option)
+        eventScroll(sidebarM, constPosition, documentM, option)
     });
 }
